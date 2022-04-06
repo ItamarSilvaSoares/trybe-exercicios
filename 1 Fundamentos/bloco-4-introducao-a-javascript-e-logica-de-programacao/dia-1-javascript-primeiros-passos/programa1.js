a0de03bfa47ba2b4c4bd541627dcab4639d27e0d
Itamar Soares <Itamar_Soares@zohomail.com>
2022-04-06 15:02:31 -0300

    Criando programa de numero 1 e readmes

---
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa1.js
@@ -0,0 +1,24 @@
+let number1 = 1,
+    number2 = 2
+
+adicao(a, b)
+
+function adicao(a, b) {
+    console.log(a + b);
+}
+
+function subtracao(a, b) {
+    console.log(a - b)
+}
+
+function multiplicacao(a, b) {
+    console.log(a * b)
+}
+
+function divisao(a, b) {
+    console.log(a / b)
+}
+
+function modulo(a, b) {
+    console.log(a % b)
+}
