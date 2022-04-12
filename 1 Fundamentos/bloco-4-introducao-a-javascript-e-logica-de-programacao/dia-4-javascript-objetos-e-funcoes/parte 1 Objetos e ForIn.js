b0d45732dd036e6ac10c4e420e567c7c71e0e1df
Itamar Soares <102383279+ItamarSilvaSoares@users.noreply.github.com>
2022-04-12 09:50:23 -0300

    Parte 2 dos exercicos de JavaScript

---
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte 1 Objetos e ForIn.js
@@ -0,0 +1,73 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+let info2 = {
+  personagem: "Margarida e Tio Patinhas",
+  origem:
+    "Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas",
+  recorrente: "Ambos",
+};
+
+//   1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
+console.log(`Bem-vinda, ${info.personagem}`);
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+console.log(info);
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto.
+
+for (const key in info) {
+  console.log(key);
+}
+
+// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+
+for (const key in info) {
+  console.log(info[key]);
+}
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+for (const key in info2) {
+  console.log(info2[key]);
+}
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+    {
+      titulo: "Harry Potter e o Prisioneiro de Azkaban",
+      autor: "JK Rowling",
+      editora: "Rocco",
+    },
+  ],
+};
+
+//   6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+console.log(
+  `O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`
+);
+
+//   7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
+
+// 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+console.log(
+  `${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos`
+);
---
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte 2 Funcoes.js
@@ -0,0 +1,123 @@
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+console.log("\n" + verificaPalindrome("arinra") + "\n");
+
+function verificaPalindrome(string) {
+  for (let index = 0; index < string.length / 2; index += 1) {
+    if (string[index] !== string[string.length - 1 - index]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function criaAray() {
+  let array = [];
+  for (let index = 0; index < 6; index++) {
+    array.push(Math.floor(Math.random() * 60) + 1);
+  }
+  return array;
+}
+
+let array = criaAray();
+
+function maiorValor(array) {
+  let maiorNumber = array[0];
+  let numberMaiorIndex = null;
+  for (const key in array) {
+    if (array[key] > maiorNumber || array[key] === maiorNumber) {
+      maiorNumber = array[key];
+      numberMaiorIndex = key;
+    }
+  }
+  return numberMaiorIndex;
+}
+
+console.log(array);
+console.log(maiorValor(array) + "\n");
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+function menorValor(array) {
+  let maiorNumber = array[0];
+  let numberMaiorIndex = null;
+  for (const key in array) {
+    if (array[key] < maiorNumber || array[key] === maiorNumber) {
+      maiorNumber = array[key];
+      numberMaiorIndex = key;
+    }
+  }
+  return numberMaiorIndex;
+}
+
+console.log(array);
+console.log(menorValor(array));
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+let nomes = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"];
+
+function maiorNome(nomes) {
+  let maiorNome = nomes[0];
+  for (let index = 0; index < nomes.length; index++) {
+    if (nomes[index].length > maiorNome.length) {
+      maiorNome = nomes[index];
+    }
+  }
+  return maiorNome;
+}
+console.log("\n" + nomes);
+console.log(maiorNome(nomes) + "\n");
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+let numeros = [2, 3, 3, 5, 8, 2, 3];
+
+function numberRepeated(numbers) {
+  let contador = 0;
+  let repetido = 0;
+  for (let index = 0; index < numbers.length; index++) {
+    let aux = 0;
+    for (let index2 = 1; index2 < numbers.length - 1; index2++) {
+      if (numbers[index] === numbers[index2]) {
+        aux += 1;
+      }
+    }
+    if (aux > contador) {
+      contador = aux;
+      repetido = numbers[index];
+    }
+  }
+  return repetido;
+}
+
+// 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+function somatorio(number) {
+  if (number <= 0) {
+    console.log(`Erro numero não da clase dos naturais`);
+  } else {
+    let soma = 0;
+    for (let index = 1; index <= number; index++) {
+      soma += index;
+    }
+    return soma;
+  }
+}
+
+console.log(somatorio(5) + "\n");
+
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+
+function verificaFimPalavra(word, ending) {
+  if (ending.length < word.length) {
+    for (let index = 0; index < ending.length; index++) {
+      if (word[word.length - ending.length + index] !== ending[index])
+        return false;
+    }
+    return true;
+  } else return `A palavra ${ending} é maior, ou igual que a palavra  ${word}`;
+}
+
+console.log(verificaFimPalavra("trybe", "be"));
