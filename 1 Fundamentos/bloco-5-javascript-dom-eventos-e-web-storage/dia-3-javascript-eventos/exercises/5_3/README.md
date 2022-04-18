85fc132c573636ad8554ebb7cf6bcd0a0e1ba7d1
Itamar Soares <102383279+ItamarSilvaSoares@users.noreply.github.com>
2022-04-18 15:18:10 -0300

    Exercicio 1 JavaScript Eventos

---
+++ 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/5_3/README.md
@@ -0,0 +1,3 @@
+# Dia 3
+
+O objetivo dos exercí­cios desse dia foi colocar em prática o que havia aprendido sobre Elementos , DOM e eventos com pratica dos metodos de criar Elementos, adicionar Elementos e adicionar e manipular eventos.
---
+++ 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/5_3/script.js
@@ -0,0 +1,47 @@
+function createDaysOfTheWeek() {
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+const ulDay = document.querySelector("#days");
+function criaDias() {
+  const dezDaysList = [
+    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
+  let sexta = 4;
+  for (const dia of dezDaysList) {
+    let li = document.createElement("li");
+    li.className = "day";
+    if (dia === 24 || dia === 25 || dia === 31) {
+      li.className += " holiday";
+    }
+    if (dia === sexta) {
+      sexta += 7;
+      li.className += " friday";
+    }
+    li.innerText = dia;
+    ulDay.appendChild(li);
+  }
+}
+criaDias();
