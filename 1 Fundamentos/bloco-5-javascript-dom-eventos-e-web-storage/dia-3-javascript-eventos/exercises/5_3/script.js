091b9aaea66bc21acb4d73ac0f0ac0bcc8edcebe
Itamar Soares <102383279+ItamarSilvaSoares@users.noreply.github.com>
2022-04-18 15:36:08 -0300

    Exercicio 2 JavaScript Eventos: botão Feriados

--- 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/5_3/script.js
+++ 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/5_3/script.js
@@ -23,8 +23,8 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
-const ulDay = document.querySelector("#days");
 function criaDias() {
+  const ulDay = document.querySelector("#days");
   const dezDaysList = [
     29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
     20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
@@ -45,3 +45,11 @@ function criaDias() {
   }
 }
 criaDias();
+
+function botaoFeriado(string) {
+  let botaoFeriado = document.createElement("button");
+  botaoFeriado.setAttribute("id", "btn-holiday");
+  botaoFeriado.innerText = string;
+  document.querySelector(".buttons-container").appendChild(botaoFeriado);
+}
+botaoFeriado("Feriados");
