148cf9986707db3343b88ebe639c6f24a53ea08d
Itamar Soares <102383279+ItamarSilvaSoares@users.noreply.github.com>
2022-04-19 21:04:56 -0300

    exercico do dia 4, tamanho da fonte

--- 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,7 +1,9 @@
 const config = document.querySelector(".dropbtn");
 config.addEventListener("mouseover", menu);
+const ul = document.querySelector(".conf");
 
 window.onload = function () {
+  //criar as caixas com as cores de fundos
   const cores = ["white", "#0D0D0D", "#4C4C4C", "#1A1A1A"];
   const li = document.createElement("li");
   for (index = 0; index < cores.length; index += 1) {
@@ -11,22 +13,49 @@ window.onload = function () {
     div.addEventListener("click", colorBackground);
     li.appendChild(div);
   }
-  document.querySelector(".conf").appendChild(li);
+  ul.appendChild(li);
+  ranger();
 };
+
+// seletor do tamanho da fonte
+function ranger() {
+  const li = document.createElement("li");
+  const p = document.createElement("label");
+  const range = document.createElement("input");
+  range.setAttribute("id", "size");
+  p.setAttribute("for", "size");
+  p.innerText = "Font Size";
+  range.type = "range";
+  range.min = 1;
+  range.max = 2;
+  range.step = 0.1;
+  range.value = 1;
+  range.addEventListener("input", fontSize);
+  li.appendChild(p);
+  li.appendChild(range);
+  ul.appendChild(li);
+}
+
+//muda tamanho fonte
+function fontSize(event) {
+  let num = event.target.value.toString() + "em";
+  document.querySelector(".cont-artigos").style.fontSize = num;
+  console.log(num);
+}
+
 // mudar cor de fundo
 function colorBackground(event) {
   document.body.style.backgroundColor = event.target.style.backgroundColor;
   if (event.target.style.backgroundColor !== "white") {
     document.querySelector(".imagem").style.boxShadow = "0 0 15px lightgray";
-    console.log(document.querySelector(".imagem").style.boxShadow.color);
+    document.body.style.boxShadow = "0 0 8px rgb(255, 255, 255, 0.35)";
   } else {
     document.querySelector(".imagem").style.boxShadow = "0 0 15px";
+    document.body.style.boxShadow = "0 0 8px";
   }
 }
 
+// mostrar o menu
 function menu() {
-  let tes = document.createElement("li");
-  tes.innerText = "asdaad";
-  document.querySelector(".conf").appendChild(tes);
   document.querySelector(".aside").style.visibility = "visible";
 }
